e7185da2d9e99ab2f9988887c0fbc205fcb716ad
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2023-02-18 14:07:29 +0600

    balance page ui developed

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 const btnSumbit = document.getElementById("btn-submit");
-console.log(btnSumbit);
+const userEmail = document.getElementById("user-email");
+const userPassword = document.getElementById("user-password");
+
+// addevent Lister
+btnSumbit.addEventListener("click", function () {
+  const email = userEmail.value;
+  const password = userPassword.value;
+  if (email === "toki osman" && password === "toki123") {
+    console.log("valid user");
+  } else alert("invalid user");
+});
